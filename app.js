ba4a69a04b91136434c8e9a90b909d29cf0b83ab
perdanaph <danangputro9@gmail.com>
2022-12-29 20:01:24 +0700

    refactoring: using env variable

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const route = require('./routers/index');
+
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'ok',
-  });
-});
+app.use(route);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log('Server run on port 3000');
 });
